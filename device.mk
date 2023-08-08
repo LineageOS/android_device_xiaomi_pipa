0ed8e9631ba0f5257090b52705777234c4cc75ff
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-08-08 22:33:47 +0200

    pipa: Add Xiaomi Peripheral Manager
     * right now keyboardutils just does disable the keyboard input device after boot
    
    Change-Id: I7020b043e9e643d125e9641b18f3f0e71d8343b1

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Peripheral Manager
+PRODUCT_PACKAGES += \
+    XiaomiPeripheralManager
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
